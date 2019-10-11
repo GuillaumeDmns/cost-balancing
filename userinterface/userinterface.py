3bac9227b3f8be68f4e30b4e8e59ba4009ebef79
Guillaume Damiens <guillaume.damiens@etu.utc.fr>
2019-10-11 17:03:41 +0200

    Add about window and LICENSE

--- userinterface/userinterface.py
+++ userinterface/userinterface.py
@@ -23,19 +23,16 @@ class MainWindow:
 
         self.people_menu = Menu(self.menu_bar, tearoff=0)
         self.people_menu.add_command(label="Add", command=lambda: self.open_new_person_window())
-        self.people_menu.add_command(label="Edit")
-        self.people_menu.add_command(label="Delete")
         self.menu_bar.add_cascade(label="People", menu=self.people_menu)
 
         self.expense_menu = Menu(self.menu_bar, tearoff=0)
         self.expense_menu.add_command(label="Add")
-        self.expense_menu.add_command(label="Delete")
         self.menu_bar.add_cascade(label="Expenses", menu=self.expense_menu)
 
         self.help_menu = Menu(self.menu_bar, tearoff=0)
         self.help_menu.add_command(label="Contribute",
                                    command=lambda: open_web_browser("https://github.com/GuillaumeDmns/cost-balancing"))
-        self.help_menu.add_command(label="About")
+        self.help_menu.add_command(label="About", command=self.open_about_window)
         self.menu_bar.add_cascade(label="Help", menu=self.help_menu)
 
         self.window.config(menu=self.menu_bar)
@@ -61,6 +58,9 @@ class MainWindow:
 
         # New person window
         self.new_person_window = None
+
+        # About window
+        self.about_window = None
 
         # Mainloop
         self.window.mainloop()
@@ -100,10 +100,16 @@ class MainWindow:
             self.list_of_people.remove(person)
             self.update_people_list()
 
+    def open_about_window(self):
+        self.about_window = Toplevel(self.window)
+        AboutWindow(self.about_window)
+        self.about_window.transient(self.window)
+        self.about_window.grab_set()
+        self.window.wait_window(self.about_window)
+
 
 class PersonWindow:
     def __init__(self, window):
-        # Edit window
         self.window = window
         self.window.geometry("300x300")
         self.window.resizable(0, 0)
@@ -119,7 +125,7 @@ class PersonWindow:
         self.balance = Label(self.balance_frame, text=str(self.new_balance.get()) + "€")
 
         self.buttons_frame = Frame(self.window)
-        self.cancel_button = Button(self.buttons_frame, text="Cancel", command=lambda: close_app(self.window))
+        self.cancel_button = Button(self.buttons_frame, text="Cancel", command=lambda: self.window.destroy())
 
         self.buttons_frame.pack(side="bottom", pady=15)
         self.cancel_button.pack(side=LEFT, padx=5, pady=5)
@@ -169,3 +175,22 @@ class NewPersonWindow(PersonWindow):
         self.list_of_people.append(People(self.new_name.get(), self.new_balance.get()))
         self.window.destroy()
 
+
+class AboutWindow:
+    def __init__(self, window):
+        self.window = window
+        self.window.resizable(0, 0)
+
+        self.license = Label(self.window, text="MIT License", fg="blue", cursor="hand2")
+        self.license.pack(side="top", padx=10, pady=5)
+        self.license.bind("<Button-1>", lambda e: open_web_browser("https://choosealicense.com/licenses/mit/"))
+
+        self.frame_made_by = Frame(self.window)
+        self.made_by = Label(self.frame_made_by, text="Made with ♥ by")
+        self.made_by.pack(side=LEFT, padx=0, pady=0)
+        self.me = Label(self.frame_made_by, text="Guillaume Damiens", fg="blue", cursor="hand2")
+        self.me.pack(side=RIGHT, padx=0, pady=0)
+        self.me.bind("<Button-1>", lambda e: open_web_browser("https://github.com/GuillaumeDmns"))
+        self.frame_made_by.pack(padx=10, pady=10)
+
+        self.window.title("About cost-balancing")
